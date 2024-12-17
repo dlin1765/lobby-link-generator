0bb7dad8b3f860b32fc9a82fb56cdce34014b836
Daniel Lin <dannylin1765@gmail.com>
2024-12-16 16:35:23 -0800

    Create test script
    queries my steam profile for my lobby link

---
+++ test-script.py
@@ -0,0 +1,68 @@
+from pynput import keyboard
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = 'https://steamcommunity.com/id/bongbobg/'
+class MyException(Exception): pass
+
+def on_release(key):
+    print(key.char +  ' released\n')
+    if key == keyboard.Key.esc:
+        return False
+
+def on_activate():
+    print('trying to access steam profile')
+    joinGameTag = ''
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            print(type(soup))
+            gameStatus = soup.find_all('div', {"class": "profile_in_game"})
+            if len(list(gameStatus[0].children)) == 3:
+                print('no game open or not online')
+            else:
+                for child in gameStatus[0].children:
+                    if(str(child) != '\n' and child['class'][0] == 'profile_in_game_joingame'):
+                        joinGameTag = child
+                if len(list(joinGameTag.contents)) == 1:
+                    print('no lobby detected')
+                else:
+                    print('lobby detected')
+                    print(joinGameTag.contents[1]['href'])
+                return None
+        else:
+            print('Error:', response.status_code)
+            return None
+    except MyException as e:
+        print(e.args[0])
+
+def for_canonical(f):
+    if f == keyboard.Key.esc:
+        return False
+    return lambda k: f(l.canonical(k))
+
+hotkey = keyboard.HotKey(
+    keyboard.HotKey.parse('<ctrl>+<alt>+c'),
+    on_activate)
+with keyboard.Listener(
+        on_press=for_canonical(hotkey.press),
+        on_release=for_canonical(hotkey.release)) as l:
+    l.join()
+
+
+# def on_press(key):
+#     try:
+#         print(key.char + " key pressed\n")
+#     except AttributeError:
+#         print("special key {key} pressed\n")
+
+# def on_release(key):
+#     print(key.char +  ' released\n')
+#     if key == keyboard.Key.esc:
+#         return False
+
+# # Collect events until released
+# with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+#     listener.join()
