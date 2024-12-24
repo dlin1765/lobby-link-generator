e9e198c0b83b983e4cd87fcc586cb07efe408d35
Daniel Lin <dannylin1765@gmail.com>
2024-12-24 12:39:43 -0800

    add dist directory

--- lobby-generator.py
+++ lobby-generator.py
@@ -52,13 +52,13 @@ def for_canonical(f):
     return lambda k: f(l.canonical(k))
 
 hotkey = keyboard.HotKey(
-    keyboard.HotKey.parse('<ctrl>+<alt>+c'),
+    keyboard.HotKey.parse('<ctrl>+c'),
     on_activate)
 with keyboard.Listener(
         on_press=for_canonical(hotkey.press),
         on_release=for_canonical(hotkey.release)) as l:
     l.join()
-
+print('lobby generator running, press ctrl c while in a lobby to get your steam lobby link')
 
 # def on_press(key):
 #     try:
